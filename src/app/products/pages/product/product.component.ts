a4943aa6b2b4fbd7aaa9ae873c5ca848ac2486da
Silvia Lozano <sylvilozano@gmail.com>
2024-06-06 13:48:54 +0200

    feature: initial poject setUp

---
+++ src/app/products/pages/product/product.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'products-product-page',
+  templateUrl: './product.component.html',
+  styleUrl: './product.component.scss',
+})
+export class ProductComponent {}
